9cd7d0035dceb2f512f39c1ea831bd35bc864541
onu-khatri <85816412+onu-khatri@users.noreply.github.com>
2024-03-12 15:02:43 +0530

    add sample data.js

---
+++ js/data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    id: "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1FQzlYZjhlRWtjWSZhYl9jaGFubmVsPUFrc2hhdFNocml2YXN0YXZh",
+    name: "Test-Youtube",
+    root: "VmlkZW8=",
+    generic: "TlNGVw==",
+  },
+  {
+    id: "aHR0cHM6Ly91bnNwbGFzaC5jb20vcy9waG90b3MvYXJ0LWdhbGxlcnk=",
+    name: "unSplash-gallery",
+    root: "SW1hZ2U=",
+    generic: "TlNGVw==",
+  }
+];
